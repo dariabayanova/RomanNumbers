aedaddbf08240167cb69ff0f0040de523aaf6446
dasha <d.bayanova@dodopizza.com>
2019-05-15 17:06:24 +0300

    Refactor to constant array

--- RomanNumber.Domain/RomanNumber.cs
+++ RomanNumber.Domain/RomanNumber.cs
@@ -4,18 +4,8 @@ namespace RomanNumber.Domain
     {
         public static string Convert(int number)
         {
-            if (number == 1)
-            {
-                return "I";
-            }
-            else if (number == 2)
-            {
-                return "II";
-            }
-            else
-            {
-                return "III";
-            }
+            var romanNumbers = new [] {"I", "II", "III"};
+            return romanNumbers[number - 1];
         }
     }
 }
