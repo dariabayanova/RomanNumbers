6fc56363c6a70ea0f3f59183c1102a991beed863
dasha <d.bayanova@dodopizza.com>
2019-05-19 16:54:31 +0300

    Rewrite all algorithm

--- RomanNumber.Domain/RomanNumber.cs
+++ RomanNumber.Domain/RomanNumber.cs
@@ -9,35 +9,9 @@ namespace RomanNumber.Domain
             var FIVE = 5;
             var TEN = 10;
             var FIFTY = 50;
-            
-            if (number < ONE)
-            {
-                return "";
-            }
-            if (number >= ONE && number <= THREE * ONE)
-            {
-                return "I" + Convert(number - ONE);
-            }
-      
-            if (number < FIVE)
-            {
-                return Convert(number - THREE) + "V";
-            }
-            if (number >= FIVE && (number <= FIVE + THREE * ONE))
-            {
-                return "V" + Convert(number - FIVE);
-            }
 
-            if (number < TEN)
-            {
-                return Convert(number - FIVE - THREE) + "X";
-            }
-            if (number >= TEN && number < TEN + THREE * TEN)
-            {
-                return "X" + Convert(number - TEN);
-            }
 
-            if (number < FIFTY)
+            if (number >= 40 && number <= 49)
             {
                 return "XL" + Convert(number - 40);
             }
@@ -46,6 +20,20 @@ namespace RomanNumber.Domain
                 return "L" + Convert(number - FIFTY);
             }
 
+            if (number >= TEN - ONE)
+            {
+                return Convert(TEN - number) + "X" + Convert(number - TEN);
+            }
+            
+            if (number >= FIVE - ONE)
+            {
+                return Convert(FIVE - number) + "V" + Convert(number - FIVE);
+            }
+            if (number >= ONE)
+            {
+                return "I" + Convert(number - ONE);
+            }
+            
             return "";
         }
     }
